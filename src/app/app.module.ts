f41dac01c990c7205e7666e6c133047eb74863a4
Fabio1981OG <fabioogoncalves@hotmail.com>
2022-03-08 13:14:55 -0300

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { InputButtonComponent } from './input-button/input-button.component';
+import { InputTextComponent } from './input-text/input-text.component';
+import { InputNumberComponent } from './input-number/input-number.component';
+import { InputSubmitComponent } from './input-submit/input-submit.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    InputButtonComponent,
+    InputTextComponent,
+    InputNumberComponent,
+    InputSubmitComponent,
+
   ],
   imports: [
     BrowserModule
